89e2b02c4287e2c5448697311343a661db7caecc
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-04-26 17:38:43 -0700

    wrote code for all items component.

---
+++ item_cart/app/assets/javascripts/components/_all_items.js.jsx
@@ -0,0 +1,13 @@
+var AllItems = React.createClass({
+  componentDidMount() {
+    console.log("Component mounted");
+  },
+
+  render() {
+    return (
+      <div>
+        <h1>All items component</h1>
+      </div>
+    );
+  }
+});
